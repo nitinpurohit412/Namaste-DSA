4bc7e18f0036fa0f9c0f9bbbb53117f9684a7a04
Nitin Purohit <nitinpurohit412@gmail.com>
2026-09-24 15:17:44 +0530

    Function and if-else

---
+++ app.js
@@ -0,0 +1,47 @@
+//*  Function
+
+// function greet(name){
+//     console.log("Namaste ", name)
+// }
+// let x = "Nitin Purohit"
+// greet(x)
+
+//*
+
+// function multiply(a,b){
+//     console.log(a  * b)
+// }
+
+// multiply(5, 6)
+
+//*
+// function square(a){
+//     let result = a * a
+//     return result
+// }
+
+// let value = square(89)
+// console.log(value)
+
+//* Eligible to vote or not
+
+// function eligibleToVote(age) {
+//   if (age < 1) {
+//     console.log("Invalid input");
+//   } else if (age < 18) {
+//     console.log("Not Eligible to vote");
+//   } else {
+//     console.log("Eligible to vote");
+//   }
+// }
+// eligibleToVote(9);
+
+//*  check if number is even or odd
+
+// function checkEvenOdd(num) {
+//   if (num % 2 == 0) {
+//     console.log("Even");
+//   } else console.log("Odd");
+// }
+// checkEvenOdd(8);
+ 
