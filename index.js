be2453684ebf37170f932c5a53e24bc5ab46c4af
Nitin Purohit <nitinpurohit412@gmail.com>
2026-09-24 14:34:47 +0530

    First class

---
+++ index.js
@@ -0,0 +1,37 @@
+//* String.
+
+// let firstName = "Eren"
+// let lastName = "Jeager"
+
+// let fullName = firstName + ' ' + lastName
+// console.log(fullName)
+
+
+//*  Array.
+
+// let arr = [1,5,9,7,3,6,8,4]
+// let sum = arr[0] + arr[3]
+// console.log(sum)
+// console.log(arr[3])
+
+//* 
+
+// let arr = ['makima', 'mikasa', 'armin', 'eren']
+// console.log(arr[3])
+
+
+//* 
+
+// let arr = ['ippo', 7, [10, 2,['hercules',8]]]
+// console.log(arr[2][2][0])
+
+//*   Object.
+
+// let obj = {
+//     a : 7,
+//     firstName : "Eren",
+//     lastName : "Jeager",
+//     bool : true,
+//     arr : [1,2,3]
+// }
+// console.log(obj.firstName + obj.lastName)
