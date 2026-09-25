eaa5a520a5f8febdca0a752a544f1fbee94d0fe3
Nitin Purohit <nitinpurohit412@gmail.com>
2026-09-25 16:08:11 +0530

    Loops - for and while

---
+++ Class-003/app.js
@@ -0,0 +1,25 @@
+//* Loops
+
+//* for loop
+
+// for(let i=1; i<=5; i++){
+//     console.log("Hello world " + i)
+// }
+
+//*
+
+// let arr = [1, 5, 9, 7, 8, 20, 60, 55];
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] % 2 == 0) {
+//     console.log(arr[i]);
+//   }
+// }
+
+
+//* while loop
+
+// let i=0
+// while(i<5){
+//     console.log('hello')
+//     i++
+// }
