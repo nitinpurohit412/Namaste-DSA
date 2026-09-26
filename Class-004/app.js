377d68b3c069450d032b22baf1f9f7bf7b588a9c
Nitin Purohit <nitinpurohit412@gmail.com>
2026-09-26 15:24:07 +0530

    find largest number, negative

---
+++ Class-004/app.js
@@ -0,0 +1,43 @@
+//* Write a fnc that searches for an element in an array and return the index, if the element is not present than it will return -1
+
+// let arr = [5,6,7,2,4,3]
+// function searchElement(arr, x){
+//     for(let i=0; i<arr.length;i++){
+//          if(arr[i]==x) return i;
+//     }
+//     return -1
+// }
+
+// let result = searchElement(arr, 9)
+// console.log(result)
+
+
+//* Write an fnc that returns the number  of negative numbers in the array.
+
+// let arr = [3,-9,-5,4,8,-10]
+
+// function returnNegativenum(arr){
+//    let count = 0
+//     for(let i=0;i<arr.length;i++){ 
+//         if(arr[i] < 0) count++
+//     }
+//     return count
+// }
+// let res = returnNegativenum(arr)
+// console.log(res)
+
+
+//* Write a function that returns the largest number on the array.
+
+// let arr = [5,0,7,10,8,17,1]
+
+
+// function largestNumber(arr){
+//    let largest = -1
+//    for(let i=0; i<arr.length;i++){
+//     if(arr[i] > largest) largest = arr[i]
+//    }
+//    return largest
+// }
+// let res = largestNumber(arr)
+// console.log(res)
